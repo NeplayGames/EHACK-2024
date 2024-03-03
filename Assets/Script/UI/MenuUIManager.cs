8b1b8ba6eafa6e5cfc4a1d8dec63f67888e2684f
Najshree <119293730+Najshree@users.noreply.github.com>
2024-03-03 07:15:08 -0600

    Updated game with music

--- Assets/Script/UI/MenuUIManager.cs
+++ Assets/Script/UI/MenuUIManager.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class MenuUIManager : MonoBehaviour
 {
+    [SerializeField] private GameObject musicHolder;
     // Start is called before the first frame update
     void Start()
     {
-        
+        DontDestroyOnLoad(musicHolder);
     }
 
     // Update is called once per frame
