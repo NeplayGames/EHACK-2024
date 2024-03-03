d126382c2520e1bc7d6ff2bbc793b605c35e811f
Najshree <119293730+Najshree@users.noreply.github.com>
2024-03-03 07:19:13 -0600

    Added game mechanics to start and end game from menu

--- Assets/Script/UI/MenuUIManager.cs
+++ Assets/Script/UI/MenuUIManager.cs
@@ -1,19 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuUIManager : MonoBehaviour
 {
     [SerializeField] private GameObject musicHolder;
+    [SerializeField] private Button startGame;
+    [SerializeField] private Button quitGame;
     // Start is called before the first frame update
     void Start()
     {
         DontDestroyOnLoad(musicHolder);
+        startGame.onClick.AddListener(StartGame);
+        quitGame.onClick.AddListener(EndGame);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void EndGame()
     {
-        
+       Application.Quit();
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void OnDestroy(){
+        startGame.onClick.AddListener(StartGame);
+        quitGame.onClick.AddListener(EndGame);
     }
 }
